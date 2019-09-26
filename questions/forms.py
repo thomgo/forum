aa5676137f35cb2554719996c4ccdd3798eba37b
Thomas <tg.59290@gmail.com>
2019-09-26 14:30:18 +0200

    style user topics page

--- questions/forms.py
+++ questions/forms.py
@@ -7,3 +7,8 @@ class TopicForm(ModelForm):
     class Meta:
         model = Topic
         fields = ['title', 'problem']
+
+    def __init__(self, *args, **kwargs):
+        super(TopicForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update({'class': 'form-control',})
+        self.fields['problem'].widget.attrs.update({'class': 'form-control',})
