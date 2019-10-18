ba932925d63d74c59fec9342f5b7709f6a33e06c
Thomas <tg.59290@gmail.com>
2019-10-18 17:05:14 +0200

    add docstring for index view and change questio_id in topic_id for consistency reason

--- questions/views.py
+++ questions/views.py
@@ -9,12 +9,13 @@ from .models import Topic
 
 @login_required
 def index(request):
+    """Home view after login to display all unsolved and solved topics"""
     topics = Topic.objects.filter(is_solved=False)
-    unsolved_topics = Topic.objects.filter(is_solved=True)
-    return render(request, "index.html", {"topics": topics, "unsolved_topics": unsolved_topics})
+    solved_topics = Topic.objects.filter(is_solved=True)
+    return render(request, "index.html", {"topics": topics, "solved_topics": solved_topics})
 
 @login_required
-def single(request, question_id):
+def single(request, topic_id):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         form = MessageForm(request.POST)
@@ -22,9 +23,9 @@ def single(request, question_id):
         if form.is_valid():
             message = form.save(commit=False)
             message.author = request.user
-            message.topic = Topic.objects.get(id=question_id)
+            message.topic = Topic.objects.get(id=topic_id)
             message.save()
-    topic = Topic.objects.prefetch_related('message_set').get(id=question_id)
+    topic = Topic.objects.prefetch_related('message_set').get(id=topic_id)
     form = MessageForm()
     return render(request, "single.html", {"topic": topic, 'form': form})
 
@@ -56,9 +57,9 @@ def archives(request):
     return render(request, "archives.html", {"topics": topics, 'other_topics': other_topics})
 
 @login_required
-def delete_question(request, question_id):
+def delete_question(request, topic_id):
     try:
-        topic = Topic.objects.get(id=question_id)
+        topic = Topic.objects.get(id=topic_id)
         if request.user == topic.author:
             topic.delete()
     except Exception as e:
@@ -66,9 +67,9 @@ def delete_question(request, question_id):
     return redirect('user_questions')
 
 @login_required
-def solve_question(request, question_id):
+def solve_question(request, topic_id):
     try:
-        topic = Topic.objects.get(id=question_id)
+        topic = Topic.objects.get(id=topic_id)
         if request.user == topic.author:
             topic.is_solved = 1
             topic.save()
